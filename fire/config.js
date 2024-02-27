afb1377681119c5472d65e6a3fe7b1912780e330
AarnoLempinen <aarno.antti@hotmail.com>
2024-02-27 15:54:05 +0200

    valmis

--- fire/config.js
+++ fire/config.js
@@ -6,12 +6,12 @@ import { getFirestore,collection,addDoc,serverTimestamp } from 'firebase/firesto
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxQR5nNmvFpTZEBH72j0l33Vo0isUQGqM",
-  authDomain: "fir-d3175.firebaseapp.com",
-  projectId: "fir-d3175",
-  storageBucket: "fir-d3175.appspot.com",
-  messagingSenderId: "688768350104",
-  appId: "1:688768350104:web:6424c07f1bda9dbdaf9ded"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 // Initialize Firebase
